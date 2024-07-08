60cc726a2c6bf32e4bacc431b05174941824a07a
ranggarozandi <149073767+ranggarozandi@users.noreply.github.com>
2024-07-09 01:34:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -24,4 +25,5 @@ def predict():
     return render_template('index.html', result=f'Customer ID: {cust_id} belongs to Cluster: {cluster[0]}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
