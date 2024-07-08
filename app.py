833d3b5b6c4bf487cc116c3f8948ea3ed749e966
ranggarozandi <149073767+ranggarozandi@users.noreply.github.com>
2024-07-09 01:01:21 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the Gaussian Mixture Model
+with open('gmm_model.pkl', 'rb') as file:
+    gmm_model = pickle.load(file)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    cust_id = request.form['cust_id']
+    balance = float(request.form['balance'])
+    purchases = float(request.form['purchases'])
+
+    input_data = np.array([[balance, purchases]])
+    cluster = gmm_model.predict(input_data)
+
+    return render_template('index.html', result=f'Customer ID: {cust_id} belongs to Cluster: {cluster[0]}')
+
+if __name__ == '__main__':
+    app.run(debug=True)
